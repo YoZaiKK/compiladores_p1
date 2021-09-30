ebdcb011a72ef3acce0f0102a3d9329e67b896d9
Guillermo <memocargo@hotmail.com>
2021-09-29 23:26:38 -0500

    Ya funciona con el 1.txt

--- main.py
+++ main.py
@@ -1,34 +1,37 @@
-def funcion(
-    transiciones, cadena, caracteres_validos, edo  # Recibimos parametros
-):
-    caracteres_no_validos = []
-    for caracter in cadena:
-        if caracter in caracteres_validos:  # Si el caracter esta entre los caracterers validos, entra al if
-            edo = validar(transiciones, caracter, edo)
-        else:
-            caracteres_no_validos.append(caracter)
-    print(f'Los caracteres no validos en la cadena son: {caracteres_no_validos}')
+def funcion(transiciones, cadena, caracteres_validos, edo):  # Recibimos parametros
+    edo = validar(transiciones, cadena, edo, 0, caracteres_validos)
     return edo  # Retornamos estado final
 
 
-def validar(transiciones, car, edo):
-    estados = []
-    i = 0
-    #edo = '3' 
-    for x in transiciones:
-        if x[0] == edo: 
-            break
-        i+=1 
-    if i < len(transiciones):
-        while transiciones[i][0] == edo:
-            if car != transiciones[i][2]:
+def validar(transiciones, cad, edo, index, caracteres_validos):
+    print('Entrando en funcion')
+    i = 0 
+    if index < len(cad):
+        if cad[index] in caracteres_validos: 
+            print(f'{edo}')
+            estados = []
+            print(f'i = {i}')
+            while i < len(transiciones):
+                if transiciones[i][0] == edo:
+                    # Hacemos la revision en el intervalo hecho antes
+                    if cad[index] == transiciones[i][2]:
+                        estados.append(validar(transiciones, cad, transiciones[i][4], index+1, caracteres_validos)) 
                 i += 1
-            else:
-                estados.append(transiciones[i][4])
-                i+=1
-    print(estados)
-    return edo
+            print(f'i = {i}')
+            return estados
+        else:
+            print(f'i = {i}')
+            return validar(transiciones, cad, edo, index+1, caracteres_validos)
+    else:
+        print(f'i = {i}')
+        return edo
 
+def imprimir(cadenas, estados_aceptables):
+    for x in cadenas:
+        if x in estados_aceptables:
+            print("\033[1;32m"+str(x)) 
+        else:
+            print("\033[1;31m"+str(x)) 
 
 # leer arcchivo txt
 f = open('1.txt', 'r')
@@ -55,7 +58,7 @@ for caracter in mensaje:
 mensaje = f.readline()
 for caracter in mensaje:
     if caracter != ',' and caracter != '\n':
-        EdoInicial = caracter 
+        EdoInicial = caracter
 
 # Metemos los estados finales a nuestra lista de estadosFinales
 mensaje = f.readline()
@@ -81,7 +84,8 @@ print(f'\u03A3: {Sigma}')
 """
 aux = len(EstadosQ)
 print(aux)
-""" 
+"""
 
 Estado_final = funcion(Sigma, input("Introduzca cadena: "), Epsilon, EdoInicial)
 print(f'{Estado_final}')
+# imprimir(Estado_final,EdosAceptacion)
